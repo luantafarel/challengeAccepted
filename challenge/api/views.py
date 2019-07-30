de219e307521c9945f5fcd638702f5495a21cd11
Luan Tafarel <luan.tafarel@softruck.com>
2019-07-30 00:55:24 -0300

    [ADJUST] Adjusted the repository and reconfigured the migrations

--- challenge/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import User, Address, Debt
-from .serializers import UserSerializer, AddressSerializer, DebtSerializer
-
-# Create your views here.
-class UserList(generics.ListCreateAPIView):
-    serializer_class = UserSerializer
